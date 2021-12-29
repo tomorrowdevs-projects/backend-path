c5c01aa0539b04e0ae86a362bafa6149749acb82
rz <rz@sanchopanza>
2021-12-29 15:45:19 +0100

    fix: now /api/users works well with both GET and POST methods

--- projects/004-exercise-tracker/solutions/robertozecchini/exercise_tracker/api/models.py
+++ projects/004-exercise-tracker/solutions/robertozecchini/exercise_tracker/api/models.py
@@ -1,10 +1,20 @@
+import uuid
 from django.db import models
+from django.utils.http import int_to_base36
+
+
+ID_LENGTH = 25
+
+def id_gen() -> str:
+    """Generates random string whose length is `ID_LENGTH`"""
+    return int_to_base36(uuid.uuid4().int)[:ID_LENGTH]
 
 class User(models.Model):
     username = models.CharField(max_length=128)
-    _id = models.CharField(max_length=128)
+    _id = models.CharField(max_length=ID_LENGTH, primary_key=True, default=id_gen, editable=False)
 
 class Exercise(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=256)
     duration = models.IntegerField()
     date = models.CharField(max_length=64)
