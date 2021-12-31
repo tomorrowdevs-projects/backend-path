3d346afa9caf568229850727556d708cd58f2753
rz <rz@sanchopanza>
2021-12-31 17:56:14 +0100

    feat: automatically set today as date if not provided

--- projects/004-exercise-tracker/solutions/robertozecchini/exercise_tracker/api/views.py
+++ projects/004-exercise-tracker/solutions/robertozecchini/exercise_tracker/api/views.py
@@ -8,6 +8,7 @@ from .serializers import UserSerializer, ExerciseSerializer
 from .models import User, Exercise
 import json
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -16,16 +17,22 @@ class UserViewSet(viewsets.ModelViewSet):
 
 @csrf_exempt 
 def CreateExercise(request, _id):
-    print("I'm here")
     if request.method == 'GET':
         return Response("GET not implemented", status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'POST':
         try:
             u = User.objects.get(pk = _id)
-            print(request.body)
-            data = json.loads(request.body)         #why data is not working???
-            exercise = Exercise(user = u, description = data['description'], date = data['date'], duration = data['duration'])
+            data = json.loads(request.body)
+            date = data.get('date', datetime.now().strftime('%a %b %d %Y'))
+            exercise = Exercise(user = u, description = data['description'], date = date, duration = data['duration'])
             exercise.save(force_insert = True)
-            return JsonResponse({'description': exercise.description, 'duration': exercise.duration, 'date': exercise.date})
+            return_value = {
+                'username': u.username,
+                'description': exercise.description,
+                'duration': exercise.duration,
+                'date': exercise.date,
+                '_id': u._id,
+            }
+            return JsonResponse(return_value)
         except:
             return Response("exercise creation fails", status=status.HTTP_400_BAD_REQUEST)
