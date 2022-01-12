5094b339aa94d49aec46bf75d04a8f0376c0a061
rz <rz@sanchopanza>
2022-01-12 12:02:36 +0100

    feat: implemented post editing as in example

--- projects/007-role-based-access-control/solutions/robertozecchini/role_based_access_control/blog/urls.py
+++ projects/007-role-based-access-control/solutions/robertozecchini/role_based_access_control/blog/urls.py
@@ -1,10 +1,13 @@
 # blog/urls.py
 
 from django.urls import path
-from .views import AboutPageView, BlogListView, BlogDetailView
+from .views import AboutPageView, BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
     path('about/', AboutPageView.as_view(), name='about'),
 ]
