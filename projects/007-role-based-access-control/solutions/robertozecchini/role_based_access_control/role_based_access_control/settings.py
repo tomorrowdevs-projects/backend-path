9bcd59a4e8455a4c66af1cad22e43fbceeab6de9
rz <rz@sanchopanza>
2022-01-22 05:35:37 +0100

    feat: add sign_in and sign_out

--- projects/007-role-based-access-control/solutions/robertozecchini/role_based_access_control/role_based_access_control/settings.py
+++ projects/007-role-based-access-control/solutions/robertozecchini/role_based_access_control/role_based_access_control/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
