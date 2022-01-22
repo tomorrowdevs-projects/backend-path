1b55c3798d86846dd910a80766f630aa63a83059
rz <rz@sanchopanza>
2022-01-22 06:51:44 +0100

    feat: add role based access control

--- projects/007-role-based-access-control/solutions/robertozecchini/role_based_access_control/blog/urls.py
+++ projects/007-role-based-access-control/solutions/robertozecchini/role_based_access_control/blog/urls.py
@@ -1,7 +1,7 @@
 # blog/urls.py
 
 from django.urls import path
-from .views import AboutPageView, BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from .views import AboutPageView, BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, RegisteredPageView, PayingPageView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
@@ -9,5 +9,7 @@ urlpatterns = [
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('registered/', RegisteredPageView.as_view(), name='registered'),
+    path('paying/', PayingPageView.as_view(), name='paying'),
     path('about/', AboutPageView.as_view(), name='about'),
 ]
--- projects/007-role-based-access-control/solutions/robertozecchini/role_based_access_control/blog/views.py
+++ projects/007-role-based-access-control/solutions/robertozecchini/role_based_access_control/blog/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
+from django.http import HttpResponse, HttpResponseForbidden
 from .models import Post
+import django.http as ht
+
+def must_be_paying(user):
+    return user.groups.filter(name='paying').count()
+
+class PayingUserMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        try:
+            return self.request.user.user_type == 'paying'
+        except:
+            return False
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
@@ -11,7 +26,7 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post_detail.html'
 
@@ -29,3 +44,9 @@ class BlogDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
+
+class RegisteredPageView(LoginRequiredMixin, TemplateView):
+    template_name = 'only_registered.html'
+
+class PayingPageView(PayingUserMixin, TemplateView):
+    template_name = 'only_paying.html'
