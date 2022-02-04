0a77d5c6a8c85aa9658d7a362b17e2eef590dbdb
rz <rz@sanchopanza>
2022-02-04 15:00:22 +0100

    feat: access token required

--- projects/008-jwt-authentication/solutions/robertozecchini/app.py
+++ projects/008-jwt-authentication/solutions/robertozecchini/app.py
@@ -1,8 +1,64 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import jwt
+import datetime
+
+key = "our_really_secret_key"
+users = {
+        "user1": "password1",
+        "user2": "password2"
+        }
+access_duration = 500
+refresh_duration = 15
 
 app = Flask(__name__)
 
-@app.route("/")
+def create_token(username, t_type = "access", duration = 5):
+    payload = {
+              "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds = duration),
+              "iat": datetime.datetime.utcnow(),
+              "id": username,
+              "type": t_type
+              }
+    encoded = jwt.encode(payload, key, algorithm = "HS256")
+    return encoded
+
+@app.route("/login", methods = ["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username in users and users[username] == password:
+        access_token = create_token(username, "access", access_duration)
+        refresh_token = create_token(username, "refresh", refresh_duration)
+        return jsonify({"access_token": access_token, "refresh_token": refresh_token})
+    else:
+        return jsonify({"error": "invalid username or password"})
+
+@app.route("/", methods = ["GET"])
 def hello_user():
-    user = "Pippo"
-    return jsonify({'Hello': user})
+    token = None
+    if "x-access-tokens" in request.headers:
+        token = request.headers["x-access-tokens"]
+    elif "authorization" in request.headers:
+        token = request.headers["authorization"]
+    if not token:
+        return jsonify({"error": "missing token"})
+    elif token.startswith("Bearer "):
+        token = token[7:]
+    try:
+        print(token)
+        payload = jwt.decode(token, key, algorithms = ["HS256"])
+        t_type = payload["type"]
+        if t_type != "access":
+            return jsonify({"error": "Invalid token type"})
+        user = payload["id"]
+        if user in users:
+            return jsonify({"Hello": user})
+        else:
+            return jsonify({"error": "Invalid token id"})
+    except jwt.ExpiredSignatureError:
+        return jsonify({"error": "Signature expired"})
+    except:
+        return jsonify({"error": "Invalid token"})
+
+if __name__ == "__main__":
+    app.run()
