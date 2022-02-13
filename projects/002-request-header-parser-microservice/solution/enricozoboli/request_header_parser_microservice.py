aec3708f45e825e29ec079dbffd4f42812d44321
Enrico <enricozob@gmail.com>
2022-02-13 21:41:09 +0100

    First commit. Add base structure

---
+++ projects/002-request-header-parser-microservice/solution/enricozoboli/request_header_parser_microservice.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+from encodings import utf_8
+
+class RequestHandler(BaseHTTPRequestHandler):
+
+    def request(self):
+        pass
+
+    def response(self):
+        pass
+
+    def convert_in_json(self):
+        pass
+
+    def do_GET(self):
+        pass
