a4f29575db0dd15fd01bd01d1a7dda4b8ad07e8b
Enrico <enricozob@gmail.com>
2022-02-13 22:43:58 +0100

    add: response, convert_to_json and do_GET methods

--- projects/002-request-header-parser-microservice/solution/enricozoboli/request_header_parser_microservice.py
+++ projects/002-request-header-parser-microservice/solution/enricozoboli/request_header_parser_microservice.py
@@ -2,16 +2,40 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 from encodings import utf_8
 
-class RequestHandler(BaseHTTPRequestHandler):
 
-    def request(self):
-        pass
+class RequestHandler(BaseHTTPRequestHandler):
+    """
+    A class to create a request handler object
+    """
 
     def response(self):
-        pass
+        """
+        Function that write a response for the client
+        """
+        return self.wfile.write(bytes(self.convert_to_json(), 'utf-8'))
 
-    def convert_in_json(self):
-        pass
+    def convert_to_json(self):
+
+        return json.dumps({
+            self.client_address[0]: 'ipaddress',
+            self.headers['Accept-Language']: 'language',
+            self.headers['User-Agent']: 'software'
+        })
 
     def do_GET(self):
-        pass
+        if self.path == '/whoami':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.response()
+
+
+def main():
+    PORT = 8000
+    server = HTTPServer(('', PORT), RequestHandler)
+    print(f'Server running on port{PORT}')
+
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
