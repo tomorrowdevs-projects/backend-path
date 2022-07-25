ab26ff8e086b44f1a68609f517daad66e6c897dd
fesiddi <sfdev25@gmail.com>
2022-07-25 23:02:36 +0200

    [FEAT] add basic unit testing with jest

--- projects/004-exercise-tracker/federicosiddi/server.js
+++ projects/004-exercise-tracker/federicosiddi/server.js
@@ -1,35 +1,15 @@
 const env = require('dotenv').config();
-const express = require('express');
-const app = express();
-const port = process.env.PORT;
 const connectDB = require('./config/dbConn');
 const mongoose = require('mongoose');
-const path = require('path');
+const PORT = process.env.PORT;
+const app = require('./app');
 
 //Connect to mongoDB
 connectDB();
 
-//middleware to handle json
-app.use(express.json());
-
-// middleware to handle urlencoded form data
-app.use(
-    express.urlencoded({
-        extended: false,
-    })
-);
-
-// serve static file
-app.use('/', express.static(path.join(__dirname, '/public')));
-
-// default route
-app.use('/', require('./routes/root'));
-// api routes
-app.use('/api', require('./routes/api/api'));
-
 mongoose.connection.once('open', () => {
     console.log('Connected to the DB');
-    app.listen(port, () => {
-        console.log(`Exercise tracker app listening on port ${port}!`);
+    app.listen(PORT, () => {
+        console.log(`Exercise tracker app listening on port ${PORT}!`);
     });
 });
