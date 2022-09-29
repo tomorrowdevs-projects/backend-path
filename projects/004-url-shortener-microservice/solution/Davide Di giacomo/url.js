8ecf40f4bfef638aac9140ccb6e29cd6bc002fa7
DjGiakma94 <davidedigiacomo94@gmail.com>
2022-09-29 11:25:16 +0200

    solution 004-url-shortener-microservice

---
+++ projects/004-url-shortener-microservice/solution/Davide Di giacomo/url.js
@@ -0,0 +1,16 @@
+const url = [
+    {
+        original_url : 'https://www.tomorrowdevs.com',
+        short_url : 1
+    },
+    {
+        original_url : 'https://www.youtube.com',
+        short_url : 2
+    },
+    {
+        original_url : 'https://github.com/DjGiakma94', 
+        short_url : 3
+    }
+]
+
+module.exports = {url}
