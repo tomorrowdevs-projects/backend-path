fe9070b781da0aed7c445c5b4d405fcbeb54fa8d
DjGiakma94 <davidedigiacomo94@gmail.com>
2022-09-29 11:04:26 +0200

    solution 001

---
+++ projects/001-calculator-with-apis/solution/Davide Di Giacomo/operation.js
@@ -0,0 +1,20 @@
+const operation = [
+    {
+        id: "+",
+        name: "Sum"
+    },
+    {
+        id: "-",
+        name: "Subtraction"
+    },
+    {
+        id: "*",
+        name: "Multiply"
+    },
+    {
+        id: "divi",
+        name: "Division"
+    }
+]
+
+module.exports = {operation}
