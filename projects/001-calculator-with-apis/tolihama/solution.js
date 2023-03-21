28d0037802ab80e63f6d6121f7e41718d3d4d43b
Francesco Marino <fra070@gmail.com>
2023-03-21 12:56:43 +0100

    node.js server setup

---
+++ projects/001-calculator-with-apis/tolihama/solution.js
@@ -0,0 +1,14 @@
+const express = require("express"); 
+
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
+// For testing purposes 
+app.get("/", (req, res) => { 
+    res.send("<h2>It's Working!</h2>"); 
+}); 
+
+// Run server
+app.listen(PORT, () => { 
+    console.log(`API is listening on port ${PORT}`); 
+});
