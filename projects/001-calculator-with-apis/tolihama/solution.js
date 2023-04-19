61a5e0e887fb1ca4ca1f135c975f8544ca363e23
Francesco Marino <fra070@gmail.com>
2023-04-19 12:33:59 +0200

    add multiply and division endpoints

--- projects/001-calculator-with-apis/tolihama/solution.js
+++ projects/001-calculator-with-apis/tolihama/solution.js
@@ -1,6 +1,8 @@
 // Dependencies
 const express = require("express"); 
 const bodyParser = require('body-parser');
+
+// Custom dependencies
 const validator = require('./validatorClass');
 
 // Express App config
@@ -10,11 +12,6 @@ const PORT = process.env.PORT || 3000;
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// For testing purposes 
-app.get("/", (req, res) => { 
-    res.send("<h2>It's Working!</h2>"); 
-}); 
 
 
 // Calculator APIs
@@ -56,13 +53,39 @@ app.post("/subtraction", (req, res) => {
 
 app.post("/multiply", (req, res) => {
     const reqBody = req.body;
+
+    const errors = validator.validate(reqBody, {
+        'multiplier': 'requested|number',
+        'multiplicand': 'requested|number',
+    });
+
+    if(errors) {
+        res.status(400).send({errors});
+        return;
+    }
+
+    res.status(200).send({
+        'result': req.body.multiplier * req.body.multiplicand,
+    });
 });
 
 app.post("/division", (req, res) => {
     const reqBody = req.body;
+
+    const errors = validator.validate(reqBody, {
+        'dividend': 'requested|number',
+        'divisor': 'requested|number',
+    });
+
+    if(errors) {
+        res.status(400).send({errors});
+        return;
+    }
+
+    res.status(200).send({
+        'result': req.body.dividend * req.body.divisor,
+    });
 });
-
-
 
 // Run server
 app.listen(PORT, () => { 
