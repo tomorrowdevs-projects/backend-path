adc2fa6c251379acac0058df385d2d6da47743f1
Francesco Marino <fra070@gmail.com>
2023-04-28 17:33:37 +0200

    Set endpoint without param (current time)

--- projects/002-timestamp-microservice/tolihama/solution.js
+++ projects/002-timestamp-microservice/tolihama/solution.js
@@ -8,6 +8,23 @@ const bodyParser = require('body-parser');
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
+// API Route timestamp microservice
+// No params: current time
+app.get('/api', (req, res) => {
+    const now = new Date();
+
+    res.status(200).send({
+        unix: now.getTime(),
+        utc: now.toUTCString(),
+    });
+});
+
+// Date param
+app.get('/api/:date', (req, res) => {
+    const date = req.params.date;
+
+    res.status(200).send({ date });
+});
 
 // Run server
 app.listen(PORT, () => { 
