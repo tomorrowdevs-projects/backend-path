7ea6af1d4f00f33d1590b74411f2b79308ae583e
Francesco Marino <fra070@gmail.com>
2023-05-22 11:07:42 +0200

    node project setup

---
+++ projects/004-url-shortener-microservice/tolihama/solution.js
@@ -0,0 +1,66 @@
+// Dependencies
+const express = require("express"); 
+const bodyParser = require('body-parser');
+
+// Express App config
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// GLOBAL VARS
+const shortenedUrlsList = [];
+
+// Calculator APIs
+// Create New Shortened Url Route
+app.post("/api/shorturl", (req, res) => {
+    const reqBody = req.body;
+
+    // Check if exist 'original_url' key in body request
+    if(!reqBody.hasOwnProperty('original_url')) {
+        res.status(400).send({
+            error: "Missing 'original_url' key in body request.",
+        });
+        return;
+    }
+
+    const originalUrl = reqBody.original_url;
+    // Check if 'original_url' is a string with the correct format
+    if(isValidUrl(originalUrl)) {
+        res.status(400).send({
+            error: "invalid url",
+        });
+        return;
+    }
+
+    // If request is validated, add 'original_url' in shortenedUrlsList
+    shortenedUrlsList.push(originalUrl);
+
+    console.log(shortenedUrlsList)
+
+    res.status(200).send({
+        original_url: originalUrl,
+        short_url: shortenedUrlsList.length,
+    });
+});
+
+// Shortened Url Redirect Route
+app.get("/api/shorturl", (req, res) => {
+
+});
+
+// Run server
+app.listen(PORT, () => { 
+    console.log(`API is listening on port ${PORT}`); 
+});
+
+const isValidUrl = urlString => {
+    try { 
+        return Boolean(new URL(urlString)); 
+    }
+    catch(e){ 
+        return false; 
+    }
+}
