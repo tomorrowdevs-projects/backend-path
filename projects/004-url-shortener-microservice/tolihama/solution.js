a38af68a298e813a1080baa72fce90c521cb4b2a
Francesco Marino <fra070@gmail.com>
2023-05-25 15:18:32 +0200

    remove old code in comments

--- projects/004-url-shortener-microservice/tolihama/solution.js
+++ projects/004-url-shortener-microservice/tolihama/solution.js
@@ -14,50 +14,7 @@ app.use(bodyParser.json());
 // Use routes from routes files
 app.use('/', routes);
 
-// Calculator APIs
-// Create New Shortened Url Route
-/*app.post("/api/shorturl", (req, res) => {
-    const reqBody = req.body;
-
-    // Check if exist 'original_url' key in body request
-    if(!reqBody.hasOwnProperty('original_url')) {
-        res.status(400).send({
-            error: "Missing 'original_url' key in body request.",
-        });
-        return;
-    }
-
-    const originalUrl = reqBody.original_url;
-    // Check if 'original_url' is a string with the correct format
-    if(isValidUrl(originalUrl)) {
-        res.status(400).send({
-            error: "invalid url",
-        });
-        return;
-    }
-
-    // If request is validated, add 'original_url' in shortenedUrlsList
-    shortenedUrlsList.push(originalUrl);
-
-    console.log(shortenedUrlsList)
-
-    res.status(200).send({
-        original_url: originalUrl,
-        short_url: shortenedUrlsList.length,
-    });
-});*/
-
-
 // Run server
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
 });
-
-/*const isValidUrl = urlString => {
-    try { 
-        return Boolean(new URL(urlString)); 
-    }
-    catch(e){ 
-        return false; 
-    }
-}*/
