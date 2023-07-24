b2e7cd6547d97393136237d79735ea6e6ba0b955
Gabriele Gabellone <gabellonegabriele@gmail.com>
2023-07-24 11:23:25 +0200

    updated swagger docs

--- projects/008-role-based-access-control/gabrielegabellone/views/index.py
+++ projects/008-role-based-access-control/gabrielegabellone/views/index.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask_login import login_required, current_user
+from flasgger import swag_from
 
 from views.auth import admin_required
 
@@ -8,12 +9,14 @@ bp = Blueprint('index', __name__)
 
 @bp.route("regular-area")
 @login_required
+@swag_from("../docs/regular_area.yml")
 def regular_area():
     return f"Welcome to the regular area, {current_user.username}."
 
 
 @bp.route("admin-area")
 @login_required
+@swag_from("../docs/admin_area.yml")
 @admin_required
 def admin_area():
     return f"Welcome to the admin area, {current_user.username}."
